18aa4cd16a261ecc9fbfa7b6afc7fa92b0eb31a1
Chris Henry <cdhenry@gmail.com>
2018-03-30 02:01:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class MyValidator < ActiveModel::Validator
+class TitleValidator < ActiveModel::EachValidator
   def validate(record)
     unless !record.title.include?("Won't Believe") || !record.title.include? ("Secret") || !record.title.include? ("Top" + /\d/ ) || !record.title.include? ("Guess")
       record.errors[:title] << false
