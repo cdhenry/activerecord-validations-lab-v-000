47e8f39987b1ed8e528a602c85f184057b21963e
Chris Henry <cdhenry@gmail.com>
2018-03-30 01:57:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
-class MyValidator < ActiveModel::Validator
-  def validate(record)
-    unless !record.title.include?("Won't Believe") || !record.title.include?("Secret") || !record.title.include?("Top" + /\d/ ) || !record.title.include?("Guess")
-      record.errors[:title] << false
-    end
-  end
-end
+# class MyValidator < ActiveModel::Validator
+#   def validate(record)
+#     unless !record.title.include?("Won't Believe") || !record.title.include?("Secret") || !record.title.include?("Top" + /\d/ ) || !record.title.include?("Guess")
+#       record.errors[:title] << false
+#     end
+#   end
+# end
 
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validates_with MyValidator
+  validates :title, inclusion: { in: %w('Won't Believe' 'Secret' 'Guess' 'Top /\d/')}
 end
